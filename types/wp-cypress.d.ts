4ad29e1a6300fc895e98e88c3de7b1ed34fbbabb
Dan Jones <dan.rh.jones@gmail.com>
2021-09-22 16:17:39 +0100

    add first function

---
+++ types/wp-cypress.d.ts
@@ -0,0 +1,4 @@
+// Type definitions for [~THE LIBRARY NAME~] [~OPTIONAL VERSION NUMBER~]
+// Project: [~THE PROJECT NAME~]
+// Definitions by: [~YOUR NAME~] <[~A URL FOR YOU~]>
+declare function installPlugin(a: string): string;
